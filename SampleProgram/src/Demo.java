8571595728c0a4c64ef86faaf997dd174d3054a3
nisum <nisum@192.168.2.6>
2017-11-16 23:13:05 +0530

    kkkk

--- SampleProgram/src/Demo.java
+++ SampleProgram/src/Demo.java
@@ -3,8 +3,6 @@ public class Demo {
 
 	
 	public static void main(String[] args) {
-		System.out.println("additional");
-		
 		System.out.println("Good Morning Boss");
 		for (int i = 0; i < 10; i++) {
 			System.out.println(i);
