3797e5b3d6810ecf8bf191156cc9439ff1008fab
HarishKumarGudivada <33482640+HarishKumarGudivada@users.noreply.github.com>
2017-11-18 21:08:40 +0530

    Update Demo.java
    loop
    
    hi
    
    merge test
    
    kkkk
    
    please check it
    
    Commited from terminal
    
    Update Demo.java

--- SampleProgram/src/Demo.java
+++ SampleProgram/src/Demo.java
@@ -8,18 +8,39 @@ public class Demo {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
 		System.out.println("additional");
 		
 =======
 >>>>>>> 8571595... kkkk
+=======
+>>>>>>> 8571595... kkkk
 		System.out.println("Good Morning Boss");
+=======
+>>>>>>> 287d7bc... please check it
 =======
 >>>>>>> 287d7bc... please check it
 		for (int i = 0; i < 10; i++) {
 			System.out.println(i);
 		}
 <<<<<<< HEAD
+<<<<<<< HEAD
 
+=======
+		for (int i = 0; i <10; i++) {
+			System.out.println(i);
+		}
+>>>>>>> f0d38ba... loop
+=======
+		System.out.println("say Hi");
+>>>>>>> 48c5c96... hi
+=======
+		System.out.println(" Hi merge is Tested");
+>>>>>>> f34f0d6... merge test
 =======
 		for (int i = 0; i <10; i++) {
 			System.out.println(i);
@@ -34,7 +55,12 @@ public class Demo {
 	}
 =======
 		
-		System.out.println();
+		System.out.println("Hai boss");
+}
+>>>>>>> 4a97677... Commited from terminal
+=======
+		
+		System.out.println("Hai boss");
 }
 >>>>>>> 4a97677... Commited from terminal
 }
